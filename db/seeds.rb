9c0d7194a6c3672311f8ddcbc5f5adbff718f4e3
beltranbot <beltranbot@gmail.com>
2021-07-07 21:39:07 -0500

    added users with secure password and user seeder

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+User.create(username: "Evgeny", password: "password")
+User.create(username: "Jonsnow", password: "password")
+User.create(username: "Arya", password: "password")
+User.create(username: "Frodop", password: "password")
+User.create(username: "Gandalf", password: "password")
